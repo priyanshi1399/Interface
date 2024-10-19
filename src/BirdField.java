b0e05cae1a51f0d00b00d2ed5d16b3bd57657370
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-19 17:22:01 +0530

    Fields in Interface

---
+++ src/BirdField.java
@@ -0,0 +1,17 @@
+public interface BirdField {
+
+    int MAX_HEIGHT_IN_FEET=2000;
+}
+
+//it will be equal to below Interface so field value will be same
+
+
+/*public interface BirdField {
+
+    public static final int MAX_HEIGHT_IN_FEET=2000;
+}
+*/
+
+//In first I have not written it public static final it is implicitely defined.
+//all the variables which you defined in Interface they are public they are constant.
+//they are static and final also.
