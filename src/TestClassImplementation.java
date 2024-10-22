dfb6b3afd20826584ea35d8e5eacff7739ad2c46
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-22 14:56:05 +0530

    ObjectMethodInFunctional

---
+++ src/TestClassImplementation.java
@@ -0,0 +1,4 @@
+public class TestClassImplementation implements testInterface{
+
+    //there I have not given any implementation
+}
---
+++ src/testInterface.java
@@ -0,0 +1,4 @@
+public interface testInterface {
+    //for this there is no need to implement in that class which is extending it.
+    String toString();
+}
