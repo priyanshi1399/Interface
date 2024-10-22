7d75bc022fc9fd049dba8da9d47e62fc7bfa2d61
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-22 17:36:26 +0530

    InterfaceExtendingFunctionaInterface_useCase2

---
+++ src/UseCase2_IntExtendFunctionaI.java
@@ -0,0 +1,8 @@
+@FunctionalInterface
+public interface UseCase2_IntExtendFunctionaI {
+    //This is use case of interface extending functional Interface
+
+    public boolean canBreathe();
+
+    //check in UseCase2_IntExtendFunctionalOther Interface
+}
