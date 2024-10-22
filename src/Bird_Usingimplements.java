64103d2ffa27970c30f0693c0cdd0c72aa291d6a
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-22 15:03:01 +0530

    WaysToImplementsFunctional Interface_1_usingImplemets

---
+++ src/Bird_Usingimplements.java
@@ -0,0 +1,8 @@
+@FunctionalInterface
+public interface Bird_Usingimplements {
+
+    //Example of different ways to implements functionalInterface
+    //using implements
+    void canFly(String val);
+    //check its implementation in EagleUsingImplements Class
+}
---
+++ src/EagleUsingImplements.java
@@ -0,0 +1,7 @@
+public class EagleUsingImplements implements Bird_Usingimplements {
+
+    @Override
+    public void canFly(String val){
+        System.out.println("Eagle Bird Implementation");
+    }
+}
