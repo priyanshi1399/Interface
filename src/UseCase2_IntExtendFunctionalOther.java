facc5b0407972486f716e5829873e210147c8701
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-22 17:36:37 +0530

    InterfaceExtendingFunctionaInterface_useCase2

---
+++ src/UseCase2_IntExtendFunctionalOther.java
@@ -0,0 +1,7 @@
+public interface UseCase2_IntExtendFunctionalOther extends UseCase2_IntExtendFunctionaI{
+
+    void canFly(String val);
+
+    //it is allowed because there is we haven't put functional Interface Keyword so there is no restriction
+    //this si allowed
+}
